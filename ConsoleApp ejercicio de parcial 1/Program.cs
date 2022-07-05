ba2b150c1a292035a2b21e9588cc994dd0c8e767
silvi <silvi@LAPTOP-GES52B11>
2022-07-05 19:51:37 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp ejercicio de parcial 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp_ejercicio_de_parcial_1deJordanLoisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double dolar;
+			double euro;
+			int pesos;
+
+			Console.Write("ingrese el valor de pesos");
+			pesos = int.Parse(Console.ReadLine());
+
+			Console.Write("ingrese el valor del dolar");
+			dolar = Double.Parse(Console.ReadLine());
+
+			dolar = pesos / 230;
+			euro = dolar / 1.17;
+
+			Console.WriteLine($"la cantidad de dolar que puedo comprar es { dolar}");
+			Console.WriteLine($"la cantidad de euros que puedo comprar es  {euro}");
+		}
+    }
+}
